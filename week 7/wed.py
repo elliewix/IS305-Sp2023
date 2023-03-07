e06d28de57cdd7a47999646cf1af980b3d390fd1
Elizabeth Wickes <elliewix@gmail.com>
2023-03-07 13:54:54 -0600

    wed

--- week 7/wed.py
+++ week 7/wed.py
@@ -62,15 +62,40 @@ print(data)
 
 all_chunks = []
 temp_chunk = []
+#
+# for i, f in enumerate(data):
+#     temp_chunk.append(f)
+#     if f == 'done':
+#         # temp_chunk.pop(-1) # an option to remove the last thing
+#         all_chunks.append(temp_chunk) # collect up
+#         temp_chunk = [] # clear out
+#     elif i == len(data) - 1:
+#         # we've reached the end
+#         all_chunks.append(temp_chunk)
 
+stops = ['done', 'finished', 'end']
 for i, f in enumerate(data):
-    temp_chunk.append(f)
-    if f == 'done':
+    # temp_chunk.append(f)
+    if f in stops: # if we had a list
+    # if f == 'done': # if f in stops:
+        # don't need to add anything to temp
+        # temp_chunk.pop(-1) # an option to remove the last thing
         all_chunks.append(temp_chunk) # collect up
         temp_chunk = [] # clear out
     elif i == len(data) - 1:
+        # do we need to add something to temp?
+        temp_chunk.append(f)
         # we've reached the end
         all_chunks.append(temp_chunk)
+    else:
+        temp_chunk.append(f)
 
 
-print(all_chunks)
+# print(all_chunks)
+
+
+
+nums = [352, 353, 354, 416, 417, 419, 420, 487, 488, 489, 490, 491, 505, 569, 663, 736, 740, 741, 753, 754, 810, 811, 812, 872, 873, 874, 963, 965, 969, 976, 977]
+
+
+
